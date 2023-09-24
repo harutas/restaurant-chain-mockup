697c29e8747e841f1dad0086c7ce297650cdd7bd
harutas <96802323+harutas@users.noreply.github.com>
2023-09-24 22:05:51 +0900

    create: entry point

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+// コードベースのファイルのオートロード
+spl_autoload_extensions(".php");
+spl_autoload_register(function ($class) {
+  $base_dir = __DIR__ . "/src/";
+  $file = $base_dir . str_replace("\\", "/", $class) . ".php";
+
+  if (file_exists($file)) {
+    require_once $file;
+  }
+});
+
+// composerの依存関係のオートロード
+require_once 'vendor/autoload.php';
+
+// クエリ文字列からパラメータを取得
+$min = $_GET['min'] ?? 5;
+$max = $_GET['max'] ?? 20;
+
+// パラメータが整数であることを確認
+$min = (int)$min;
+$max = (int)$max;
+
+// ユーザーの生成
+$users = Helpers\RandomGenerator::users($min, $max);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>User Profiles</title>
+  <link rel="stylesheet" href="css\styles.css">
+</head>
+
+<body>
+  <h1>User Profiles</h1>
+
+  <?php foreach ($users as $user) : ?>
+    <div class="user-card">
+      <!-- ユーザー情報の表示 -->
+      <?php echo $user->toHTML() ?>
+    </div>
+  <?php endforeach; ?>
+
+</body>
+
+</html>
